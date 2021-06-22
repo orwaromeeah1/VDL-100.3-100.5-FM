0bfbca934a3404e7e9e89aefa8e7cc1ec69b76ec
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2021-06-22 23:54:39 +0300

    episode card design

--- lib/ui/programs/page/program_details/program_details_page.dart
+++ lib/ui/programs/page/program_details/program_details_page.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:vdl/ui/programs/widget/episode_card.dart';
 
 class ProgramDetailsPage extends StatefulWidget {
   @override
@@ -7,8 +8,37 @@ class ProgramDetailsPage extends StatefulWidget {
 }
 
 class _ProgramDetailsPageState extends State<ProgramDetailsPage> {
+  double width;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: width,
+          child: Column(
+            children: [
+              ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context,int index){
+                    return EpisodeCard(
+                      image: 'https://www.vdl.me/wp-content/uploads/2021/05/Sawt-Jdid.jpg',
+                      date: '23 شباط 2020',
+                      title: 'عقد المكتب السياسي اجتماعه الاسبوعي برئاسة رئيس الحزب',
+                      episodeNumber: 'الحلقة الأولى',
+                    );
+                  }
+
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
