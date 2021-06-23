c76e323f016060410d747683c5c333b0ed485e38
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2021-06-23 14:51:21 +0300

    historical programs design

--- lib/ui/programs/page/historical_programs/historical_programs_page.dart
+++ lib/ui/programs/page/historical_programs/historical_programs_page.dart
@@ -1,1 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:vdl/ui/programs/page/program_details/program_details_page.dart';
+import 'package:vdl/ui/programs/widget/program_card.dart';
 
+class HistoricalPrograms extends StatefulWidget {
+  @override
+  _HistoricalProgramsState createState() => _HistoricalProgramsState();
+}
+
+class _HistoricalProgramsState extends State<HistoricalPrograms> {
+  double width;
+
+  @override
+  Widget build(BuildContext context) {
+    width = MediaQuery.of(context).size.width;
+
+    return  Scaffold(
+//      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            // image
+            new Column(
+              children: <Widget>[
+                new Container(
+                  height: 200,
+                  width: width,
+                  color: Colors.black87,
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'البرامج التاريخية',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            new Container(
+                alignment: Alignment.bottomCenter,
+                padding: new EdgeInsets.only(top: 200, right: 20.0, left: 20.0),
+                child:ListView.builder(
+                    itemCount: 2,
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder:  (BuildContext context, int index){
+                      return  GestureDetector(
+                        onTap: ()=> Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => ProgramDetailsPage()
+                            )
+                        ),
+                        child: Container(
+                          child: ProgramCard(
+                            image: 'https://www.vdl.me/wp-content/uploads/2021/05/Sawt-Jdid.jpg',
+                            category: 'محلية',
+                            name: 'صوت جديد',
+                            date: 'الخميس 20 تشرين الثاني',
+                          ),
+                        ),
+                      );
+                    }
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
