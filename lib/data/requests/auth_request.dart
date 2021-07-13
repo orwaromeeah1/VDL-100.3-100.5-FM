1308ce59578416dfcfe3c7b39a11fb16659113c9
Zolfekar Seleten <zolfekarseleten94@gmail.com>
2021-07-13 17:13:38 +0300

    add auth functionality and refresh token

---
+++ lib/data/requests/auth_request.dart
@@ -0,0 +1,25 @@
+class AuthRequest {
+  String grantType;
+  String clientId;
+  String clientSecret;
+
+  AuthRequest({
+    this.grantType:'client_credentials'
+    , this.clientId
+    , this.clientSecret
+  });
+
+  AuthRequest.fromJson(Map<String, dynamic> json) {
+    grantType = json['grant_type'];
+    clientId = json['client_id'];
+    clientSecret = json['client_secret'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['grant_type'] = this.grantType;
+    data['client_id'] = this.clientId;
+    data['client_secret'] = this.clientSecret;
+    return data;
+  }
+}
